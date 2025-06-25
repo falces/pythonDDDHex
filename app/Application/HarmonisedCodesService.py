6724fdf6be05c30999800979969c248d794d4091
Javier Rodríguez <javier.rodriguez@create-store.com>
2025-06-25 13:59:56 +0200

    ADD: Import harmonised codes

--- app/Application/HarmonisedCodesService.py
+++ app/Application/HarmonisedCodesService.py
@@ -1,8 +1,7 @@
 from Shared.Domain.Repositories.AbstractRepository import AbstractRepository
 from Domain.HarmonisedCodes.HarmonisedCode import HarmonisedCode
 from Domain.HarmonisedCodes.HarmonisedCodesModel import HarmonisedCodesModel
-# from flask import current_app
-from app import db
+from app import db, app
 
 
 class HarmonisedCodesService:
@@ -16,17 +15,27 @@ class HarmonisedCodesService:
         self,
     ) -> list:
         limit = 500
-        offset = 5000
-        totalFetched = 5000
-        totalAvailable = float('inf')
+        offset = 0
+        numberOfRequests = 0
 
-        while (totalFetched < totalAvailable):
+        while True:
+            numberOfRequests += 1
+            app.logger.info("Limit: %s, offset: %s", limit, offset)
             harmonisedCodesResponse = self.repository.getAllHarmonisedCodes(
                 limit = limit,
                 offset = offset,
             )
 
-            receivedHarmonisedCodes = harmonisedCodesResponse['data']
+            if "data" in harmonisedCodesResponse:
+                receivedHarmonisedCodes = harmonisedCodesResponse["data"]
+            else:
+                receivedHarmonisedCodes = harmonisedCodesResponse
+
+            app.logger.info("Received %s harmonised codes", len(receivedHarmonisedCodes))
+
+            if not receivedHarmonisedCodes:
+                app.logger.info("Total requests: %s", numberOfRequests)
+                break
 
             for receivedHarmonisedCode in receivedHarmonisedCodes:
                 id = receivedHarmonisedCode['id']
@@ -43,12 +52,8 @@ class HarmonisedCodesService:
                     description = harmonisedCode.description
                 )
                 db.session.add(harmonisedCodeModel)
+                db.session.commit()
 
-            paginationInfo = harmonisedCodesResponse['paging_info']
-            totalFetched += paginationInfo['fetched']
-            totalAvailable = paginationInfo['total']
-            offset += totalFetched
-
-            db.session.commit()
+            offset += limit
 
         return {'message': 'Harmonised codes updated successfully'}, 201
