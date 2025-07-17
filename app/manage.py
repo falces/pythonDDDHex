e13204b0601f8d7e5b4d2d1e6c7ed96073f6e4a8
falces <falces@gmail.com>
2025-07-17 12:57:36 +0200

    ADD: Improved manager. Script.

--- app/manage.py
+++ app/manage.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python
 
 import sys
+import re
+
+allowedActions = [
+    "actions",
+    "create_entity",
+]
 
 def main():
     try:
-        if len(sys.argv) > 2 or len(sys.argv) < 1:
-            raise Exception()
+        if len(sys.argv) > 2 or len(sys.argv) < 2:
+            raise Exception("Write just ONE action")
         else:
-            executeAction(sys.argv[1])
+            action = sys.argv[1]
+            if action not in allowedActions:
+                raise Exception("Action not allowed")
+            execute = getattr(sys.modules[__name__], action)
+            execute()
+
     except Exception as e:
-        print("Write just ONE action")
+        print(e)
 
+def actions():
+    print(f'Allowed actions: {allowedActions}')
 
-def executeAction(
-    action: str,
-):
-    print("Let's " + action + "!")
+def create_entity():
+    try:
+        print('Let\'s create a new entity.')
+        entity = input('Entity name: ')
+        pattern = r"^[a-zA-Z]+$"
+        if not re.fullmatch(pattern, entity):
+            raise Exception(entity + " is not a valid name.")
+        entity = entity.capitalize()
+        print(f"Let's create the entity {entity}")
+
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
     main()
